6851fd994e83072f90b1263188374819f3dc9730
github username <github email address>
2019-04-28 07:08:29 +0100

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,60 @@
+require "pry"
+
+
+
 def reformat_languages(languages)
   # your code here
+  # languages_by_style.collect do |tier_one, tier_two|
+  #   languages_by_style.invert
+  # take out of original and put into a new one
+  # new_hash = languages_by_style.collect do |tier_one, tier_two|
+  #   languages_by_style[tier_one].collect do |tier_two, tier_three|
+  #     {tier_two => tier_three}
+  array_one = [:ruby, :javascript, :python, :java]
+  array_two = [:clojure, :javascript, :erlang, :scala]
+  new_hash = {}
+  languages_by_style.each do |tier_one, tier_two|
+    languages_by_style[tier_one].each do |tier_two, tier_three|
+      new_hash[tier_two] = tier_three # if you do collect it creates arrays which you don't want!
+    end
+  end
+  another_hash = {}
+  yet_another_hash = {}
+  new_hash.each do |tier_one, tier_two|
+    if array_one.include?(tier_one)      
+      # another_hash[tier_one] = {:style => [:oo]}
+      new_hash[tier_one] = new_hash[tier_one].merge(:style => [:oo])
+    end
+    if array_two.include?(tier_one)
+      new_hash[tier_one] = new_hash[tier_one].merge(:style => [:functional]) {|key, v1, v2| v1 + v2}
+      # binding.pry
+      #  yet_another_hash[tier_one] = {:style => [:functional]}
+      # new_hash[tier_one].merge(:style => [:functional])
+    end
+  end
+  
+  # a = new_hash.merge!(another_hash) {|tier_one, v1, v2| v1}
+  
 end
+
+
+
+
+# another_hash = {}
+# yet_another_hash = {}
+# new_hash.each do |tier_one, tier_two|
+#   if array_one.include?(tier_one)
+#     # new_hash[tier_one] << {:style => [:oo]}
+#     # new_hash.merge!({:style => [:oo]})
+#     another_hash[tier_one] = {:style => [:oo]}
+#   end
+#   if array_two.include?(tier_one)
+#      yet_another_hash[tier_one] = {:style => [:functional]}
+#      # new_hash[tier_one].merge(:style => [:functional])
+#   end
+# end
+
+# a = new_hash.merge!(another_hash) {|tier_one, v1, v2| v1}
+
+# binding.pry
+# end
